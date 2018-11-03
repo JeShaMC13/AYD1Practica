d6ea777e71112af8eac7a7cf117fafb5fba914a2
Jenny Montenegro <jennysharai14@gmail.com>
2018-11-02 19:27:08 -0600

    Agregando Pruebas para Admiin-Products

---
+++ src/app/admin/admin-products/admin-products.component.spec.ts
@@ -0,0 +1,31 @@
+import { Product } from '../../models/product';
+import { AdminProductsComponent } from './admin-products.component';
+import { ProductService } from '../../services/product.service';
+import { Observable } from 'rxjs/Observable';
+
+describe('AdminProductsComponent', () => {
+  let component: AdminProductsComponent;
+  let service: ProductService;
+  const product: Product = {
+    $key: '1',
+    title: 'titulo',
+    price: 1,
+    category: 'featured',
+    imageUrl: 'https://imageURL.com'
+  };
+
+  beforeEach(() => {
+    service = new ProductService(null);
+    component = new AdminProductsComponent(service);
+    component.products = [product];
+  });
+
+  it('Deberia ser creado el componente', () => {
+    expect(service).toBeTruthy();
+    expect(component).toBeTruthy();
+  });
+
+
+
+
+});
